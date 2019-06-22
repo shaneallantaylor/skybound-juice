353c8be6889b9a53d0123077f2e2a6492c748824
Shane Taylor <shaneallantaylor@gmail.com>
2019-06-21 23:33:11 -0700

    add images, font, styles, and interaction

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
-  $('#shop-menu-item').hover(function () {
+  $('#shop-menu-item').mouseover(function () {
+    $('#shop-menu-item').addClass('highlight');
     $('.sub-menu-container').addClass('show');
-  }, function () {
-    setTimeout(function () {
-      $('.sub-menu-container').removeClass('show');
-    }, 200)
   });
-  $('.sub-menu-container').hover(function () {
-    $('.sub-menu-container').addClass('show');
-  }, function () {
+
+  $('#hero').mouseenter(function () {
     setTimeout(function () {
       $('.sub-menu-container').removeClass('show');
-    }, 200)
+      $('#shop-menu-item').removeClass('highlight');
+    }, 400)
+  });
+
+  $(".top-menu li").not("#shop-menu-item").mouseenter(function () {
+    setTimeout(function () {
+      $('.sub-menu-container').removeClass('show');
+      $('#shop-menu-item').removeClass('highlight');
+    }, 300)
+  });
+
+  $('#pencil-ad .close').click(function () {
+    $('#pencil-ad .close').css("display", "none");
+    $('#pencil-ad').addClass('closed');
   })
+
 });
